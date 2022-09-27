8335e744fc0f662dfe8a6b71afa1189171ad0174
Novo Persona <adcaldwell93@yahoo.com>
2022-09-26 20:54:38 -0400

    added for loop for events but not working

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,16 @@ var calendarTime = moment().format("8 a");
 time.textContent = calendarTime;
 
 // need to add submit button to all save button icons to save event 
+var texta = document.getElementsByClassName("texta")
+console.log(texta);
+
+for (var i = 0; i < texta.length; i++){
+texta[i].addEventListener("click", function openTextBox(){
+    console.log("ouch!");
+    var textBox = document.createElement("textarea");
+})
+}
+// need to add textbox to all events. 
 
 // work on input form for events
-
 
